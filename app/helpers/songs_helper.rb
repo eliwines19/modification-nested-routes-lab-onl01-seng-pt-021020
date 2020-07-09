8fcdf31fe4df91779138efa54928a7c50047e0c4
Elijah wines <elijahwines84@yahoo.com>
2020-07-09 14:10:23 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,6 +3,8 @@ module SongsHelper
     if song.artist.nil?
       select_tag "song[artist_id]",
       options_from_collections_for_select(Artist.all, :id, :name)
-
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
   end
 end
